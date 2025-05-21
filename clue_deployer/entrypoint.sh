afb9551cd75a772de4da1549130f9feb89aec738
Julius Ostermann <julius@ostermann.cloud>
2025-05-21 19:33:57 +0200

    Add several patches to overcome connection issues when connecting to cluster deployed locally on the host

---
+++ clue_deployer/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+# Start Docker daemon, allowing our local registry as insecure (with interal port)
+# dockerd-entrypoint.sh --insecure-registry registry:5000 &
+
+# Patch the kubeconfig to allow access to clusters running on the host
+python3 /app/patch_kubeconfig.py
+
+# Wait until the docker daemon is ready
+while ! docker info >/dev/null 2>&1; do
+  sleep 0.2
+done
+
+export KUBECONFIG=/app/kubeconfig_patched
+
+# keep the container alive
+tail -f >/dev/null
+
+# Invoke your deployer script
+#exec uv run clue_deployer/main.py
