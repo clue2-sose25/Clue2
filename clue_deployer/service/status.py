07e4c85037de3ba93f3b2d5ef68671142e09eb29
IssaFadel <62032746+Issafadel@users.noreply.github.com>
2025-06-02 19:28:01 +0200

    Create status.py
    status enum

---
+++ clue_deployer/service/status.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Phase(str, Enum):
+    PREPARING_CLUSTER = "Preparing the cluster"
+    DEPLOYING_SUT     = "Deploying SUT"
+    WAITING           = "Waiting"
+    IN_PROGRESS       = "In progress"
+    DONE              = "Done"
