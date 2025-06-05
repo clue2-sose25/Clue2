dec9de7eb795a98c4c7f17bdca989cea131c88d3
Emil Balitzki <balitzki@b-ci.de>
2025-06-05 11:32:09 +0200

    Fixed env variables

--- clue_deployer/entrypoint.sh
+++ clue_deployer/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 # Environment variable fallbacks
-: "${AS_SERVICE:=false}"
+: "${DEPLOY_AS_SERVICE:=false}"
 : "${DEPLOY_ONLY:=false}"
 
-
-echo "Starting with deploy $DEPLOY_ONLY, service_type $AS_SERVICE, and SUT: $SUT_NAME, Experiment: $EXPERIMENT_NAME if defined :/"
+echo "Starting CLUE Deployer..."
+echo "Deploying as a service: $DEPLOY_AS_SERVICE"
+echo "Deploy without benchmarking: $DEPLOY_ONLY"
+echo "Bechmark config: SUT: `$SUT_NAME`, experiment: `$EXPERIMENT_NAME`"
 
 # Patch the kubeconfig to allow access to clusters running on the host
 python3 /app/clue_deployer/patch_kubeconfig.py
@@ -14,18 +16,21 @@ chmod 600 /app/clue_deployer/kubeconfig_patched
 
 export KUBECONFIG=/app/clue_deployer/kubeconfig_patched
 
-# If DEPLOY_ONLY env. variable is true, call the deployer script without running the experiments
-if [ "$DEPLOY_ONLY" = "true" ]; then
-    echo "Deploying the SUT without executing any experiments"
-    exec uv run clue_deployer/src/run.py
-    exit 0
-fi
-
-if [ "$AS_SERVICE" = "true" ]; then
+# If DEPLOY_AS_SERVICE = True, deploy CLUE as a service
+if [ "$DEPLOY_AS_SERVICE" = "true" ]; then
     echo "Starting FastAPI service..."
     exec uvicorn clue_deployer.service.service:app --host 0.0.0.0 --port 8000
     exit 0
 fi
-# Deploy the specified SUT and experiment
-echo "Deploying and executing selected SUT experiments"
-exec uv run clue_deployer/src/main.py
+
+# If DEPLOY_ONLY = True, deploy CLUE as a script without running the experiments
+if [ "$DEPLOY_ONLY" = "true" ]; then
+    echo "Deploying the SUT without executing any experiments"
+    exec uv run clue_deployer/src/run.py
+    exit 0
+else
+    # Deploy CLUE as a script with benchmarking
+    echo "Deploying and executing selected SUT experiments"
+    exec uv run clue_deployer/src/main.py
+    exit 0
+fi
