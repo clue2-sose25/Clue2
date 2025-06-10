361abb95b63304bf082364cd3a615936d6ac23b9
Emil Balitzki <balitzki@b-ci.de>
2025-06-10 12:45:00 +0200

    Updated control panel design

--- clue_dashboard/src/pages/ControlsPage.tsx
+++ clue_dashboard/src/pages/ControlsPage.tsx
@@ -6,12 +6,11 @@ import {InfoIcon} from "@phosphor-icons/react";
 const workloadOptions = ["shaped", "rampup", "pausing", "fixed"];
 
 const ControlsPage = () => {
-  // Define the default deployment state with initial values
   const defaultDeployment: Deployment = {
     SUT: null,
     experiment: null,
-    workload: "shaped", // Default workload
-    iterations: 1, // Default iterations
+    workload: "shaped",
+    iterations: 1,
   };
 
   const [currentDeployment, setCurrentDeployment] =
@@ -21,7 +20,6 @@ const ControlsPage = () => {
   const [experiments, setExperiments] = useState<string[]>([]);
   const [deployOnly, setDeployOnly] = useState(false);
 
-  // Fetch available SUTs and experiments on component mount
   useEffect(() => {
     fetch("/api/list/sut")
       .then((r) => r.json())
@@ -33,7 +31,6 @@ const ControlsPage = () => {
       .catch(() => setExperiments([]));
   }, []);
 
-  // Deploy function to send deployment details to the API
   const deploy = async () => {
     if (!currentDeployment.SUT || !currentDeployment.experiment) return;
     await fetch("/api/deploy/sut", {
@@ -48,27 +45,26 @@ const ControlsPage = () => {
   };
 
   return (
-    <div className="w-full h-full flex flex-col items-center gap-8 p-8">
-      {/* Header texts */}
+    <div className="w-full h-full flex flex-col items-center gap-6 pt-4">
       <div className="flex flex-col items-center">
         <p className="text-xl font-medium">Deploy CLUE</p>
-        <p className="">Choose your parameters for the benchmark</p>
+        <p>Choose your parameters for the benchmark</p>
       </div>
-      {/* Parameters selection */}
       <div className="flex flex-col gap-4 w-1/3">
         {/* SUT Dropdown */}
-        <div className="flex items-center justify-between gap-4">
-          <label className="flex gap-2 items-center text-sm font-medium mb-1 ">
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="sut-select"
+            className="flex gap-2 items-center text-sm font-medium"
+          >
             SUT <InfoIcon size={18} />
           </label>
           <select
+            id="sut-select"
             className="border p-2 w-full"
             value={currentDeployment.SUT || ""}
             onChange={(e) =>
-              setCurrentDeployment({
-                ...currentDeployment,
-                SUT: e.target.value,
-              })
+              setCurrentDeployment({...currentDeployment, SUT: e.target.value})
             }
           >
             <option value="" disabled>
@@ -82,11 +78,15 @@ const ControlsPage = () => {
           </select>
         </div>
         {/* Experiment Dropdown */}
-        <div className="flex items-center justify-between gap-4">
-          <label className="flex gap-2 items-center text-sm font-medium mb-1">
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="experiment-select"
+            className="flex gap-2 items-center text-sm font-medium"
+          >
             Experiment <InfoIcon size={18} />
           </label>
           <select
+            id="experiment-select"
             className={`border py-2 px-4 w-full ${
               !currentDeployment.SUT ? "opacity-50" : ""
             }`}
@@ -110,11 +110,15 @@ const ControlsPage = () => {
           </select>
         </div>
         {/* Workload Type Dropdown */}
-        <div className="flex items-center justify-between gap-4">
-          <label className="flex gap-2 items-center text-sm font-medium mb-1 text-nowrap">
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="workload-select"
+            className="flex gap-2 items-center text-sm font-medium text-nowrap"
+          >
             Workload Type <InfoIcon size={18} />
           </label>
           <select
+            id="workload-select"
             className="border p-2 w-full"
             value={currentDeployment.workload || ""}
             onChange={(e) =>
@@ -131,13 +135,16 @@ const ControlsPage = () => {
             ))}
           </select>
         </div>
-
         {/* Iterations Input */}
-        <div className="flex items-center justify-between gap-4">
-          <label className="flex gap-2 items-center text-sm font-medium mb-1 text-nowrap">
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="iterations-input"
+            className="flex gap-2 items-center text-sm font-medium text-nowrap"
+          >
             Iterations <InfoIcon size={18} />
           </label>
           <input
+            id="iterations-input"
             type="number"
             min="1"
             className="border p-2 w-full"
@@ -150,22 +157,22 @@ const ControlsPage = () => {
             }
           />
         </div>
-
         {/* Deploy Only Checkbox */}
-        <div className="flex justify-between">
-          <label className="inline-flex items-center gap-2">
-            <p className="flex gap-2 items-center text-sm font-medium">
-              Deploy only <InfoIcon size={18} />
-            </p>
-            <input
-              type="checkbox"
-              className="border w-4 h-4"
-              checked={deployOnly}
-              onChange={(e) => setDeployOnly(e.target.checked)}
-            />
+        <div className="flex items-center py-2 justify-between gap-2">
+          <label
+            htmlFor="deploy-only-checkbox"
+            className="flex gap-2 items-center text-sm font-medium"
+          >
+            Deploy only <InfoIcon size={18} />
           </label>
+          <input
+            id="deploy-only-checkbox"
+            type="checkbox"
+            className="border w-5 h-5"
+            checked={deployOnly}
+            onChange={(e) => setDeployOnly(e.target.checked)}
+          />
         </div>
-
         {/* Deploy Button */}
         <button
           className="rounded p-2 bg-blue-500 text-white hover:bg-blue-700"
