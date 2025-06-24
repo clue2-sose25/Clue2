547f5b103e569ab57be083b801f4d53fa113054d
Emil Balitzki <balitzki@b-ci.de>
2025-06-24 17:12:27 +0200

    Reverted changes

--- clue_builders/teastore/entrypoint.sh
+++ clue_builders/teastore/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -e
 
-# Set DNS for the container
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-
-# Start Docker daemon with the same DNS and insecure registry
-dockerd-entrypoint.sh --dns 8.8.8.8 --insecure-registry registry:5000 &
+# Start Docker daemon, allowing our local registry as insecure (with interal port)
+dockerd-entrypoint.sh --insecure-registry registry:5000 &
 
 # Wait until the docker daemon is ready
 while ! docker info >/dev/null 2>&1; do
