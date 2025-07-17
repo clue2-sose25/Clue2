b00de2224f031dacc852761a1acd693f37d65bdf
Emil Balitzki <balitzki@b-ci.de>
2025-07-17 20:39:40 +0200

    Reverted some unnecessary changes

--- clue_loadgenerator/builder/entrypoint.sh
+++ clue_loadgenerator/builder/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Start Docker daemon, allowing our local registry as insecure (with interal port)
 dockerd-entrypoint.sh --insecure-registry registry:5000 &
-# testcomment
+
 # Wait until the docker daemon is ready
 while ! docker info >/dev/null 2>&1; do
   sleep 0.2
